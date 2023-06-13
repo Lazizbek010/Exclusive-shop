1a3e16defcd893102ff9c19eca4180bd47cc29e2
Abduqodir0205 <abduqodirDeveloper@gmail.com>
2023-06-13 06:34:51 -0700

    Abduqodir

---
+++ src/components/SectionTop.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="feature">
+        <div class="feature__top">
+            <div class="feature-top__card"></div>
+            <p>Featured</p>
+        </div>
+        <h1>New Arrival</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.feature__top {
+    width: 20px;
+    height: 40px;
+    background-color: #DB4444;
+    display: flex;
+    gap: 30px;
+}
+
+p {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+    /* identical to box height, or 125% */
+
+
+    /* Secondary 2 */
+
+    color: #DB4444;
+}
+
+h1 {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 36px;
+    line-height: 48px;
+    /* identical to box height, or 133% */
+
+    letter-spacing: 0.04em;
+
+    /* Text2 */
+
+    color: #000000;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,41 @@
 <template>
-    <div>
-      <header>Header</header>
-      <main></main>
-      <footer>
+  <div>
+    <header>Header</header>
+    <main>
+      <section id="arrival">
+        <div class="container">
+          <div class="arrival__content">
+            <div class="arrival-content__top">
+              <SectionTop />
+            </div>
+            <div class="arrival-content__bottom">
+              <div class="arrival-bottom__left">
+                <img class="arrival-bottom__img" src="../assets/images/png/Play-station.png" alt="">
+                <div class="arrival-bottom__text">
+                  <h3 class="arrival-bottom__h3">PlayStation 5</h3>
+                  <p class="arrival-bottom__p">Black and White version of the PS5 coming out on sale.</p>
+                  <a href="#" class="arrival-bottom__a">Shop Now</a>
+                </div>
+              </div>
+              <div class="arrival-bottom__right">
+                <div class="card"></div>
+                <div class="card"></div>
+                <div class="card"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <footer>
 
-      </footer>
-    </div>
+    </footer>
+  </div>
 </template>
 
 <script setup>
+import SectionTop from '../components/SectionTop.vue';
 
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
