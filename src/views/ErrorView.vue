543d4b8eb9780451f94251c0e232952b4d180515
Lazizbek010 <lazizbekisaqov19981110@gmail.com>
2023-06-20 12:50:15 +0500

    error page end

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="error">
-    <div class="breadcrumb">
-      <ul class="breadcrumb__list">
-        <li>
-          <router-link to="/" class="pop-14-21-400">Home</router-link>
-        </li>
-        <li>
-          <router-link to="/contact" class="pop-14-21-400 active"
-            >Contact</router-link
-          >
-        </li>
-      </ul>
-    </div>
-    <div class="error__content">
-      <h1>404 Not Found</h1>
-      <p>Your visited page not found. You may go home page.</p>
-      <router-link :to="{name: 'error'}">
-        <button>Back to home page</button>
-      </router-link>
+    <div class="container">
+      <div class="breadcrumb">
+        <ul class="breadcrumb__list">
+          <li>
+            <router-link to="/" class="pop-14-21-400">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/error" class="pop-14-21-400 active"
+              >404 Error</router-link
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="error__content">
+        <h1>404 Not Found</h1>
+        <p class="pop-16-24-400 ">Your visited page not found. You may go home page.</p>
+        <router-link to="/">
+          <button class="btn">Back to home page</button>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
